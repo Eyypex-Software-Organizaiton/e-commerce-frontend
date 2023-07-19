bf25cd050d4f4495303cc7d7f59d6c83f9e0922a
EmreCakir <118616290+Cakir010@users.noreply.github.com>
2023-07-19 20:17:42 +0300

    little changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', "./node_modules/flowbite/**/*.js"],
+  content: [   "./src/**/*.{js,jsx,ts,tsx}",  "./node_modules/flowbite/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        green: {
+          dark: "#377375",
+          light: "#DDEDE3",
+        },
+        grey: {
+          primary: "#DAD7CD",
+        },
+      },
+      fontFamily: {
+        'sans': ['Roboto', 'sans-serif'],
+      },
+    },
   },
   plugins: [require('flowbite/plugin')],
 }
+
