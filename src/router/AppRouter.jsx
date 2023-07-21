a43fbcb753417098e4731aceea7e68b7b99b5eaf
fatihozkul35 <fatihozkul3541@gmail.com>
2023-07-21 17:51:34 +0200

    breadcrumb added

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -4,17 +4,19 @@ import Navbar from '../components/navbar/Navbar'
 import Footer from '../components/footer/Footer'
 import Home from '../pages/home/Home'
 import ProductsDetails from '../pages/ProductsDetails/ProductsDetails'
+import ProductsList from '../pages/ProductsList/ProductsList'
+import Breadcrumbs from '../components/breadcrumbs/Breadcrumbs'
 
 const AppRouter = () => {
   return (
     <>
-    <Navbar/>
-    <Routes>
-<Route path='/' element={<Home/>}/>
-<Route path='/details' element={<ProductsDetails/>}/>
-
-
-    </Routes>
+      <Navbar />
+      <Breadcrumbs />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path=':slug' element={<ProductsList />} />
+        <Route path='/details' element={<ProductsDetails />} />
+      </Routes>
     <Footer/>
     </>
     
